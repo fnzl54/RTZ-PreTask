734aa75bec9762abb269734ee12813903e731fd6
fnzl54 <rnjscksdud1@naver.com>
2023-07-23 11:54:13 +0900

    feat: html에서 자막 부분 Json 추출하는  함수 추가

--- source/request_url.py
+++ source/request_url.py
@@ -1,4 +1,6 @@
 import requests
+from bs4 import BeautifulSoup
+import json
 
 def get_html_from_url(url):
     try:
@@ -10,5 +12,23 @@ def get_html_from_url(url):
         html_data = response.text
         return html_data
     except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return None
+    
+def extract_json_data_from_html(html_data):
+    try:
+        soup = BeautifulSoup(html_data, 'html.parser')
+
+        # id가 '__NEXT_DATA__'인 스크립트 태그 가져오기
+        script_tag = soup.find('script', id='__NEXT_DATA__')
+
+        # JSON 데이터 추출
+        if script_tag:
+            json_data = json.loads(script_tag.string)
+            return json_data
+        else:
+            print("'__NEXT_DATA__' tag가 존재하지 않습니다.")
+            return None
+    except json.JSONDecodeError as e:
         print(f"Error: {e}")
         return None
