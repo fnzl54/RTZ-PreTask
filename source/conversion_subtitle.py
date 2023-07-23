5f911c4850cd0922701daa3b2c882974ed85b8f2
fnzl54 <rnjscksdud1@naver.com>
2023-07-23 12:22:40 +0900

    feat: 숫자를 시간 형식으로 바꿔주는 함수 추가

--- source/conversion_subtitle.py
+++ source/conversion_subtitle.py
@@ -7,3 +7,11 @@ def make_subtitle_list (json_data):
             subtitle_list.append(temp_str)
              
     return subtitle_list
+        
+def number_to_time_format(number):
+    milliseconds = number % 1000
+    seconds = (number // 1000) % 60
+    minutes = (number // (1000 * 60)) % 60
+    hours = number // (1000 * 60 * 60)
+    
+    return f"{hours:02d}:{minutes:02d}:{seconds:02d},{milliseconds:03d}"
