b5d1c11f39ff6c25c583f73a5a7c60f99620ee58
fnzl54 <rnjscksdud1@naver.com>
2023-07-23 13:01:15 +0900

    feat: srt 파일 형식으로 변경하여 파일 생성 및 저장

--- source/conversion_subtitle.py
+++ source/conversion_subtitle.py
@@ -15,3 +15,23 @@ def number_to_time_format(number):
     hours = number // (1000 * 60 * 60)
     
     return f"{hours:02d}:{minutes:02d}:{seconds:02d},{milliseconds:03d}"
+
+def make_srt(output_file, subtitle_list):
+
+    with open(output_file, 'w', encoding='utf-8') as f:
+        subtitle_length = len(subtitle_list) - 1
+        
+        for i in range( subtitle_length ):
+            subtitle = subtitle_list[i].split('/')[0]
+            start_time = number_to_time_format(int(subtitle_list[i].split('/')[1]))
+            end_time = number_to_time_format(int(subtitle_list[i + 1].split('/')[1]) - 24)
+            
+            subtitle_line = f"{i+1}\n{start_time} --> {end_time}\n{subtitle}\n\n"
+            f.write(subtitle_line)
+        
+        subtitle = subtitle_list[subtitle_length].split('/')[0]
+        start_time = number_to_time_format(int(subtitle_list[subtitle_length].split('/')[1]))
+        end_time = number_to_time_format(int(subtitle_list[subtitle_length].split('/')[1]) + 100)
+        
+        subtitle_line = f"{i+1}\n{start_time} --> {end_time}\n{subtitle}\n\n"
+        f.write(subtitle_line)
